e2c491014456e19aee5bb9ae38a53a3055826df8
Sam <Sam@Sam-MacBook.local>
2016-08-15 13:27:02 +0800

    Update model页面

--- src/page/view/model/action.js
+++ src/page/view/model/action.js
@@ -6,14 +6,23 @@ export const RECIEVE = 'RECIEVEAMODEL'
 export const size = 10 
 
 //actionCreators
-export function fetchData(params={},success) {
+function fetchCommon(params={},success,callbackAllStatus) {
 	var url = `http://localhost:7001/providemoduledata?size=${ size }`;
 	//url参数拼接
 	url = r2fn.params(url,params);
 	return r2fetch({
 		method: 'GET',
-		successMessage: false,
-	}).dispatchFetchOne(url,requestPosts(REQUEST,'main_request'),receivePosts(RECIEVE,'main'),success)
+		callbackAllStatus,
+	}).dispatchFetchOne(url,requestPosts(REQUEST,'main'),receivePosts(RECIEVE,'main'),success)
 }
-
+//分页中使用
+var t_params; 
+export function fetchData(params={},success) {
+	t_params = params;
+	return fetchCommon(params,success)	
+}
+export function fetchPaginationData(page,success,callbackAllStatus) {
+	t_params.page = page;
+	return fetchCommon(t_params,success,callbackAllStatus)	
+}
 
--- src/page/view/model/reducer.js
+++ src/page/view/model/reducer.js
@@ -1,12 +1,16 @@
 import * as actionCreator from './action' 
+import Immutable from 'immutable'
 
 export function model(state = {}, action) {
     switch (action.type) {
-    	
 		case actionCreator.REQUEST: 
-		case actionCreator.RECIEVE: 	
-			return Object.assign({}, state,action);
-		
+			return r2fn.imutableReducer(state,action);
+		case actionCreator.RECIEVE:
+			return r2fn.imutableReducer(state,action,()=>{
+				if(action.main){
+					action.main.result.data.size = actionCreator.size;
+				}
+			});
         default:
 			return state;
     }
