b84ea82f65e471c4d69d2abb4047da8dbaf2a311
Sam <Sam@Sam-MacBook.local>
2016-09-06 19:10:10 +0800

    fixed 文件不存在访问问题

--- server.js
+++ server.js
@@ -10,14 +10,13 @@ app.use(require('webpack-dev-middleware')(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath,//必须跟webpack.config.js一致
 }));
-
+//设置路径不存在(webpack-dev-middleware内存中也不存在)时访问的目录
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('webpack-hot-middleware')(compiler));
 //地址重写，所以请求都定位到public/index.html文件
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'public/index_dev.html'));
 });
-//设置路径不存在(webpack-dev-middleware内存中也不存在)时访问的目录,不可以放在地址重写前，要不要会优先
-app.use(express.static(path.join(__dirname, 'public')));
 var host = "localhost"
 app.listen(port, host, function(err) {
     if (err) {
