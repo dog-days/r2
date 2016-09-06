9cdcee092263834aef2820e83cefe17134327a5c
Sam <Sam@Sam-MacBook.local>
2016-09-06 19:26:16 +0800

    fixed 报错问题

--- src/libs/r2/module/ModuleComponent.jsx
+++ src/libs/r2/module/ModuleComponent.jsx
@@ -14,10 +14,6 @@ var commonAction = r2ActionCreator;
 class ModuleComponent extends BasicComponent {
 	constructor(props){
 		super(props); 
-<<<<<<< HEAD
-=======
-		this.createBreadcrumb()
->>>>>>> 66adfa351d9c1956dd936c0d4ca1c1775ca4b83a
 	}
 	
 	createBreadcrumb(){
