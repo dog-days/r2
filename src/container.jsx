81aabc28320c2ba7d096caff947817cd7202a72f
Sam <Sam@Sam-MacBook.local>
2016-09-30 17:18:43 +0800

    update webpack.config.js配置 && 热替换换为react-hot-loader3

---
+++ src/container.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { Router } from 'react-router'
+import rootRoute from './routes'
+
+
+class Container extends React.Component {
+  render() {
+    const { store, history } = this.props;
+    return (
+      <Provider store={store}>
+        <Router history={history} routes={rootRoute} />
+      </Provider>	
+    );
+  }
+}
+
+
+module.exports = Container;
--- src/index.jsx
+++ src/index.jsx
@@ -1,36 +1,37 @@
-import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { Router,IndexRoute,browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux' 
+import { browserHistory } from 'react-router'
 import Immutable from 'immutable'
 //IE10等polyfill
 import polyfill from 'src/common/polyfill'
 //公共方法||类载入，用window对象访问
 import global from 'r2/global'
 import configureStore from './store'
-import rootRoute from './routes'
+import container from './container'
 
 const initialState = Immutable.Map();
 const store = configureStore(initialState,browserHistory);
 var history = syncHistoryWithStore(browserHistory, store,{
-    selectLocationState (state) {
-		return state.get('routing').toJS();
-    } 
+  selectLocationState (state) {
+    return state.get('routing').toJS();
+  } 
 })
 
-render(
-	<Provider store={store}>
-		<Router history={history} routes={rootRoute} />
-	</Provider>,	
-	document.getElementById('app_container')
-)
+function renderApp(Container) {
+  const target = document.getElementById('app_container');
+  if (target) {
+    render(
+      <Container store={store} history={history} />,
+      target
+    );
+  }
+}
 
-/*if (module.hot) {
-    require('react-hot-loader/Injection').RootInstanceProvider.injectProvider({
-        getRootInstances: function() {
-            // Help React Hot Loader figure out the root component instances on the page:
-            return [rootInstance];
-        }
-    });
-}*/
+renderApp(container);
+
+if (module.hot) {
+  module.hot.accept(
+    './container',
+    () => renderApp(require('./container'))
+  );
+}
